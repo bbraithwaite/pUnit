65f6971b7db9516a66206cd862ea79f59973806a
Bradley Braithwaite <[not>
2012-02-17 20:24:45 +0000

    added new projects

---
+++ src/ConsoleRunner/Program.cs
@@ -0,0 +1,13 @@
+﻿using pUnit;
+
+namespace ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProfileRunner runner = new ProfileRunner();
+            runner.Run();
+        }
+    }
+}
---
+++ src/Examples/ForEachTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using pUnit;
+
+namespace Examples
+{
+    [ProfileClass]
+    public class ForEachTests
+    {
+        [ProfileMethod]
+        public void ForTest()
+        {
+            List<int> list = GetNumberList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+            }
+        }
+
+        [ProfileMethod]
+        public void ForEachTest()
+        {
+            List<int> list = GetNumberList();
+
+            foreach (int i in list)
+            {
+            }
+        }
+
+        private List<int> GetNumberList()
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 1000; i++)
+            {
+                list.Add(i);
+            }
+            return list;
+        }
+    }
+}
